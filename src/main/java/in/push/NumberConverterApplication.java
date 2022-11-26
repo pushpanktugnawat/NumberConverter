7b6bf1a3001463c855baf6772cc14b65a3bd32c7
pushpank <ptu@container-xchange.com>
2022-11-26 22:52:13 +0100

    initial commit

---
+++ src/main/java/in/push/NumberConverterApplication.java
@@ -0,0 +1,13 @@
+package in.push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberConverterApplication.class, args);
+	}
+
+}
