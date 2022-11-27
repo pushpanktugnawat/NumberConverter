5b36991f571a39d17b9df657b5346494e53ad4e6
pushpank <ptu@container-xchange.com>
2022-11-27 17:53:14 +0100

    PUSH-000_Exception handling

--- src/test/java/in/push/adapter/inbound/NumberToRomanControllerTest.java
+++ src/test/java/in/push/adapter/inbound/NumberToRomanControllerTest.java
@@ -1,10 +1,8 @@
 package in.push.adapter.inbound;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import in.push.domain.enums.NumberType;
@@ -14,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -33,7 +30,7 @@ class NumberToRomanControllerTest {
     }
 
     @Test
-    void getFrontendVariables() throws Exception {
+    void should_convert_number_to_roman() throws Exception {
 
         when(numberToRomanService.convertNumberToRoman(any(), any())).thenReturn("VI");
 
